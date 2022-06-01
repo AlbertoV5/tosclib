6ba09b08c4cb139570ba3cf5f85330b3635ba27f
AlbertoV5 <avq5ac1@gmail.com>
2022-05-31 20:33:33 -0500

    updated parser :+1:

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -3,69 +3,77 @@ import tosclib as tosc
 import xml.etree.ElementTree as ET
 from tosclib.tosc import ControlElements, Property
 
-class NameParser:
+class PropertyParser:
     maxDepth = 0
     depth = 0
     
-    def __init__(self):
-        self.nodeName = ""
-        self.node = False
+    def __init__(self, targetProperty: str, depth : int = None):
+        self.targetKey = targetProperty
+        self.depthClose = depth
+        self.control = False
         self.property = False
         self.key = False
         self.value = False
-        self.name = False
+        self.targetFound = False
         self.numberOfNodes = 0
+        self.targetList = []
+        self.multiLine = ""
 
     def start(self, tag, attrib):
         self.tag = tag
         if tag == ControlElements.NODE:
             self.depth += 1
             if self.depth > self.maxDepth:
-                self.maxDepth = self.depth    
-            self.node = True
-        elif tag == ControlElements.PROPERTY and self.node:
+                self.maxDepth = self.depth
+            self.control = True
+        elif self.control and tag == ControlElements.PROPERTY:
             self.property = True
-        elif tag == Property.Elements.KEY and self.property:
+        elif self.property and tag == Property.Elements.KEY:
             self.key = True
-        elif tag == Property.Elements.VALUE and self.property:
+        elif self.property and tag == Property.Elements.VALUE:
             self.value = True
             
     def end(self, tag):
         if tag == ControlElements.NODE:
-            self.node = False
+            self.control = False
             self.depth = 1
             self.numberOfNodes += 1
-        elif tag == ControlElements.PROPERTY and self.node:
+        elif self.control and tag == ControlElements.PROPERTY:
             self.property = False
-        elif tag == Property.Elements.KEY and self.property:
+        elif self.property and tag == Property.Elements.KEY:
             self.key = False
-        elif tag == Property.Elements.VALUE and self.property:
+        elif self.property and tag == Property.Elements.VALUE:
             self.value = False
         
+        if self.targetFound and tag == Property.Elements.VALUE:
+            self.targetFound = False
+            self.targetList.append(self.multiLine)
+            self.multiLine = ""
+        
+        if self.depthClose == self.depth:
+            return self.close()
+        
     def data(self, data):
-        if data == "name" and self.node and self.property and self.key:
-            self.name = True
-        if self.node and self.property and self.name and self.value:
-            self.nodeName = data
-            print(" " * self.depth, self.maxDepth, "Name:", data)
-            self.name = False
-
+        if self.control and self.property and self.key and data == self.targetKey:
+            self.targetFound = True
+        if self.control and self.property and self.value and self.targetFound:
+            self.multiLine = f"{self.multiLine}{data}"
+            
     def close(self):
-        return self.numberOfNodes
+        return self.targetList
 
 
-def parser(element: ET.Element, target : NameParser = NameParser()):
+def parseNode(element: ET.Element, target : PropertyParser):
     start = time.process_time()
-    
     line = ET.tostring(element, encoding="unicode")
     parser = ET.XMLParser(target=target)
     parser.feed(line)
-    depth = parser.close()
+    result = parser.close()
     end = time.process_time()
-    print(depth, "-", end - start)
-
+    print("-", end - start)
+    return result
 
 root = tosc.load("docs/demos/files/Numpad_basic.tosc")
-parser(root)
+result = parseNode(root, PropertyParser(targetProperty="name", depth = 2))
 
-
+print(result)
